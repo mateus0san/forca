6c92b341c1a8f81ff242c38c7d8c0efaf4f5af38
Mateus S. Araujo <mateus0santiago@proton.me>
2025-07-06 17:18:22 -0300

    lista de palavras no código para substituir a falta de arquivo

--- src/main.c
+++ src/main.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <string.h>
 
+/* lista de palavras, caso nenhum banco de palavras seja informado (arquivo)
+   use a lista de palavras
+*/
+/* a lista será movido para um arquivo .h */
+const char *programacao[] = {
+  "gcc",
+  "git",
+  "github",
+  "python",
+  "rust",
+  "programacao orientada a objetos",
+  ""
+};
+
+int main(void) {
+  
+  // test para a lista de palavras programacao, isso será removido
+  for (int i = 0; strcmp(programacao[i], "") != 0; i++) {
+    printf("%s\n", programacao[i]);
+  }
+}
